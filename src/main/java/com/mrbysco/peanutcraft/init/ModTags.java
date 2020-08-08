5b85d4187bfe49fdacf44fd0d05681b0f8e6d088
Mrbysco <Mrbysco@users.noreply.github.com>
2020-08-08 14:08:46 +0200

    Port to 1.16.1

--- src/main/java/com/mrbysco/peanutcraft/init/ModTags.java
+++ src/main/java/com/mrbysco/peanutcraft/init/ModTags.java
@@ -1,15 +1,10 @@
 package com.mrbysco.peanutcraft.init;
 
 import net.minecraft.item.Item;
+import net.minecraft.tags.ITag;
 import net.minecraft.tags.ItemTags;
-import net.minecraft.tags.Tag;
-import net.minecraft.util.ResourceLocation;
+
 
 public class ModTags {
-    public static final Tag<Item> CROPS_PEANUT = itemTag("crop_peanut");
-
-    private static Tag<Item> itemTag(String name)
-    {
-        return new ItemTags.Wrapper(new ResourceLocation("forge", name));
-    }
+    public static final ITag.INamedTag<Item> CROPS_PEANUT = ItemTags.makeWrapperTag("forge:crop_peanut");
 }
